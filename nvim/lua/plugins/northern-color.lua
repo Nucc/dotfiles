ade6e8ecc1f34c5e19a206e0588cc7ba95c49bb0
Laszlo Papp <nucc@bteam.hu>
2024-04-29 23:31:49 +0100

    Fix Neotree preview

--- nvim/lua/plugins/northern-color.lua
+++ nvim/lua/plugins/northern-color.lua
@@ -1,6 +1,11 @@
 return {
   {
-    "nordtheme/vim",
+    "gbprod/nord.nvim",
+    lazy = false,
     priority = 1000,
+    config = function()
+      require("nord").setup({})
+      vim.cmd.colorscheme("nord")
+    end,
   },
 }
