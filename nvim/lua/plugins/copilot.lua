18e7359e50e3932a516087de766c499030f0541d
Laszlo Papp <lpapp@zendesk.com>
2025-12-02 23:47:10 +0000

    Configure Copilot keybindings for suggestion acceptance
    - Add Ctrl-A binding to accept suggestions in insert mode
    - Disable default Tab mapping to avoid conflicts with nvim-cmp
    - Add navigation keybindings for dismissing and cycling suggestions

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,19 @@
 return {
   "github/copilot.vim",
+  config = function()
+    -- Disable default Tab mapping for Copilot
+    vim.g.copilot_no_tab_map = true
+
+    -- Use Ctrl-A to accept Copilot suggestion
+    vim.keymap.set('i', '<C-a>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      silent = true
+    })
+
+    -- Copilot navigation
+    vim.keymap.set('i', '<C-]>', '<Plug>(copilot-dismiss)', { silent = true })
+    vim.keymap.set('i', '<M-]>', '<Plug>(copilot-next)', { silent = true })
+    vim.keymap.set('i', '<M-[>', '<Plug>(copilot-previous)', { silent = true })
+  end,
 }
